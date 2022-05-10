0133c98fea527b8644db7d9b6f88aa186765f9aa
Javier Lucero <javier.lucero@iess.gob.ec>
2022-05-10 10:07:46 -0500

    first commit

---
+++ src/main/java/com/capacitacion/taskbackend/controller/HelloWordController.java
@@ -0,0 +1,14 @@
+package com.capacitacion.taskbackend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWordController {
+
+	@GetMapping("/hello")
+	public String getMessage() {
+		return "Hello word";
+	}
+
+}
